228fe4f57ae303fa17beb141da6df8052c767a39
Sufiyan Yusuf <sufiyanyusuf78611@gmail.com>
2025-09-20 19:54:11 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,6 +18,10 @@ let package = Package(
 			targets: ["SwiftTea"]
 		),
 	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		.package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
+	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
